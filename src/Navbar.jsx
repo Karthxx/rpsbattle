ee5326aab18ef9b0e0b8fc5e69b13724c2f9c97c
Karthik I <karthik3105@gmail.com>
2024-08-22 15:52:26 +0100

    logo fixed

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -3,7 +3,7 @@ const Navbar = () => {
     <div className="max-w-5xl py-4 px-1 mx-auto relative ">
       <div className="w-full flex items-center justify-between ">
         <div className="flex flex-row items-center">
-          <img src="/public/rock-hand.svg" alt="" className="w-7" />
+          <img src="src/assets/rock-hand.svg" alt="" className="w-7" />
           <p className="text-lg font-bold text-white">RpsBattle.io</p>
         </div>
       </div>
