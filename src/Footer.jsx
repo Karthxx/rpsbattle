8fd8567a4a1dacddee342db169c40139c1ed7f71
Karthik I <karthik3105@gmail.com>
2024-08-22 15:56:17 +0100

    fixed logo v2

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,9 +1,11 @@
+import logo from "./assets/rock-hand.svg";
+
 const Footer = () => {
   return (
     <div className="max-w-5xl py-4 px-1 mx-auto relative ">
       <div className="w-full flex justify-center">
         <div className="flex flex-row items-center">
-          <img src="src/assets/rock-hand.svg" alt="" className="w-7" />
+          <img src={logo} alt="" className="w-7" />
           <p className="text-xs sm:text-lg font-medium text-white">
             Copyright © 2024 RpsBattle.io. All Rights Reserved.
           </p>
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,9 +1,11 @@
+import logo from "./assets/rock-hand.svg";
+
 const Navbar = () => {
   return (
     <div className="max-w-5xl py-4 px-1 mx-auto relative ">
       <div className="w-full flex items-center justify-between ">
         <div className="flex flex-row items-center">
-          <img src="src/assets/rock-hand.svg" alt="" className="w-7" />
+          <img src={logo} alt="" className="w-7" />
           <p className="text-lg font-bold text-white">RpsBattle.io</p>
         </div>
       </div>
