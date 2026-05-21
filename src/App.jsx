3a812f330485e875163801dc6d885dd7f67da79b
Karthik I <karthik3105@gmail.com>
2026-05-21 11:59:50 +0530

    fb1

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
     <AuthProvider>
       <Helmet>
         <title>
-          Play Rock Paper Scissors Online at rpsbattle.io | Free Game
+          Play Rock Paper Scissors Online at rpsbattle.io | Free Game Online
         </title>
         <meta
           name="description"
